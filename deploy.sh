07d900086967b7baecbcb1e3cc2b27d5a85a11ae
Jitse Boonstra <jitse@sent.com>
2023-07-28 21:41:05 +0200

    autogen: update site

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while true; do
+    read -p "Do you want to proceed deploying (y/n)? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+bundle exec jekyll build
+git stash
+git checkout prod
+git stash pop
+mv ./_site/404/index.html ./_site/404.html
+git --git-dir=.git --work-tree=_site add --all
+git --git-dir=.git --work-tree=_site commit -m "autogen: update site"
+echo "now push via the GitHub app"
