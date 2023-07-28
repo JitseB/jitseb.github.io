5963f3c28a7f0a7dd25f1c62e57dbd8b163b6dc4
Jitse Boonstra <jitse@sent.com>
2023-07-28 21:46:00 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    read -p "Are you on the production branch (y/n)? " yn
+    read -p "Do you want to proceed deploying (y/n)? " yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -8,7 +8,12 @@ while true; do
     esac
 done
 bundle exec jekyll build
+git stash
+git checkout prod
+git stash pop
 mv ./_site/404/index.html ./_site/404.html
 git --git-dir=.git --work-tree=_site add --all
 git --git-dir=.git --work-tree=_site commit -m "autogen: update site"
-echo "now push via the GitHub app"
+echo "now push the prod branch via the GitHub app"
+git reset --hard && git clean -f -d
+git checkout master
