a38ffef9d86e1a2e90d1dd0228b0e2ef7c5ee99a
Jitse Boonstra <jitse@sent.com>
2023-07-28 21:36:08 +0200

    Add safety net for this idiot

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+while true; do
+    read -p "Are you on the production branch (y/n)? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 bundle exec jekyll build
 mv ./_site/404/index.html ./_site/404.html
 git --git-dir=.git --work-tree=_site add --all
