17bacca72750c421c7d60bf06f4899545e0bc350
Jitse Boonstra <jitse@sent.com>
2023-07-28 21:31:08 +0200

    autogen: update site

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bundle exec jekyll build
+mv ./_site/404/index.html ./_site/404.html
 git --git-dir=.git --work-tree=_site add --all
 git --git-dir=.git --work-tree=_site commit -m "autogen: update site"
 echo "now push via the GitHub app"
